d8042ffdb50ca32857d916b5b47d3162e13c3600
black-cat-developer <rahulsrichunar@gmail.com>
2025-01-10 22:20:58 +0530

    fault resolved of experience color

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 function Navbar() {
   return (
-    <div className='text-2xl px-[10%] py-[2%] flex items-center
-    justify-between'>
-      <div className='text-4xl font-bold'>RahuL</div>
-      <div className='nav-center flex
-    justify-between gap-4 text-base font-semibold '>
-        <div>Home</div>
-        <div>About Me</div>
-        <div>Services</div>
-        <div>Portfolio</div>
-        <div>Contact</div>
+    <div
+      className=" name-container text-2xl px-[10%] py-[2%] flex items-center
+    justify-between"
+    >
+      <div className="nav_title text-4xl font-bold">RahuL</div>
+      <div
+        className="nav-center flex
+    justify-between gap-6 text-base font-semibold "
+      >
+        <div className="nav_middle">Home</div>
+        <div className="nav_middle">About Me</div>
+        <div className="nav_middle">Services</div>
+        <div className="nav_middle">Portfolio</div>
+        <div className="nav_middle">Contact</div>
       </div>
-      <button className='nav-button px-4 rounded-full text-sm bg-red-300 bg-gradient-to-r from-pink-500 to-orange-500 to-90% '>Connect with me</button>
+      <button className="nav-button px-4 rounded-full text-sm bg-red-300 bg-gradient-to-r from-pink-500 to-orange-500 to-90% ">
+        Connect with me
+      </button>
     </div>
-
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
